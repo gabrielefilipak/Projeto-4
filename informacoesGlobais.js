55d02a9cef2f57f1ac0ccda9d90d3e058ba824fc
gabrielefilipak <130515783+gabrielefilipak@users.noreply.github.com>
2024-10-08 09:43:08 -0300

    Delete informacoesGlobais.js

--- informacoesGlobais.js
+++
@@ -1,9 +0,0 @@
-const url = 'https://raw.githubusercontent.com/gabrielefilipak/Projeto-4/api/main/dados-globais.json'
-
-async function vizualizarInformacoesGlobais() {
-    const res = await fetch(url)
-    const dados = await res.json()
-    console.log(dados);
-}
-
-vizualizarInformacoesGlobais()
